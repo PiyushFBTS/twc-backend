19e583acfcb6605e9e575b9f9297c2fefe14a969
unknown <piyush.malviya@fbts.in>
2025-11-17 15:55:53 +0530

    increase-body-size

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,9 @@ const app = express();
 
 // ✅ Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 // login
 app.use("/api/auth", authRoutes);
