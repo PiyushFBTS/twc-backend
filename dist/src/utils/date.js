6c5104f8b6c2a74caba35b859e635acab69b18d6
unknown <piyush.malviya@fbts.in>
2025-11-21 17:19:33 +0530

    remove-build

--- dist/src/utils/date.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toPostgresDateTime = void 0;
-const toPostgresDateTime = (dateString) => {
-    if (!dateString)
-        return null;
-    return new Date(dateString).toISOString();
-};
-exports.toPostgresDateTime = toPostgresDateTime;
